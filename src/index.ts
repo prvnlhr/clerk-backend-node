2e9937ed9b3217def334517d9eab4138f7c82e11
Praveen Lohar <praveenlohar.in@gmail.com>
2025-05-17 10:23:28 +0530

    prisma setup and install some packages

---
+++ src/index.ts
@@ -0,0 +1,43 @@
+import express, { Application, Request, Response, NextFunction } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import router from "./routes";
+dotenv.config();
+
+const app: Application = express();
+const PORT: number = parseInt(process.env.PORT as string, 10) || 9000;
+
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://nexop-next.vercel.app",
+];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+// app.use(express.json());
+app.use(cookieParser());
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error(err.message);
+  res.status(500).json({ error: err.message });
+});
+
+app.use("/api", router);
+
+app.listen(PORT, () => {
+  console.log(`Server running on: PORT-${PORT}`);
+});
